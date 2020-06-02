bfcd8442c47bd8dadbfe8f68320d9767052afd36
kherney <kevinh939@gmail.com>
2020-06-01 19:07:04 -0500

    set static url

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main/static/'
