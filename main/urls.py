48f143c2bc3b72d0cd970ab4da0075da1db6c446
kherney <kevinh939@gmail.com>
2020-06-11 20:21:46 -0500

    Add contact_us page with class-based views.py

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.urls import path
 from django.views.generic import TemplateView
+from main import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
-    path('about_us/', TemplateView.as_view(template_name="about_us.html"), name="about_us")
+    path('about_us/', TemplateView.as_view(template_name="about_us.html"), name="about_us"),
+    # path("contact_us/", views.contact_us, name="contact_us")
+    path('contact_us/', views.ContactUsView.as_view(), name="contact_us"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.http import HttpResponseRedirect
+from main import forms
+from django.views.generic.edit import FormView
 
-# Create your views here.
+
+class ContactUsView(FormView):
+    template_name = "contact_us.html"
+    form_class = forms.ContactForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        """If the form is valid, redirect to the supplied URL."""
+        form.send_mail()
+        return super(ContactUsView, self).form_valid(form)
+
+# def contact_us(request):
+#     if request.method == "POST":
+#         form = forms.ContactForm(request.POST)
+#         if form.is_valid():
+#             form.send_mail()
+#             return HttpResponseRedirect('/')
+#     else:
+#         form = forms.ContactForm()
+#     return render(request, 'contact_us.html', {'form': form})
